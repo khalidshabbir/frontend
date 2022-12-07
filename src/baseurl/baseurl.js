12f3e667511072fe0f13df140d1b1cf1a69c563a
khalidshabbir09@gmail.com <khalidshabbir09@gmail.com>
2022-12-07 21:07:22 +0500

    first commit

---
+++ src/baseurl/baseurl.js
@@ -0,0 +1,7 @@
+export const baseurl = "https://backend-production-b11d.up.railway.app";
+// export const baseurl = "https://cuitimetable.herokuapp.com/";
+// "proxy": "https://cuitimetable.herokuapp.com/",
+// 
+
+
+// https://timetableb.herokuapp.com
